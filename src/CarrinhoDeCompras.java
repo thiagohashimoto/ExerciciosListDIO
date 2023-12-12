f347d13ecbe0c5143de309570a7232a762556074
PC Thiago <thiago.hashimoto94@gmail.com>
2023-12-11 21:59:29 -0300

    List exercise from Collections classes by DIO

---
+++ src/CarrinhoDeCompras.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+
+
+    private List<Item> carrinhoList;
+
+    public CarrinhoDeCompras() {
+        this.carrinhoList = new ArrayList<>();
+    }
+
+    public void adicionarItem(String nome, double preco, int quantidade){
+        Item item = new Item(nome,preco, quantidade);
+        this.carrinhoList.add(item);
+    };
+
+    public void removerItem(String nome){
+        List<Item> itensParaRemover = new ArrayList<>();
+        if(!carrinhoList.isEmpty()){
+            for (Item i:carrinhoList) {
+                if (i.getNome().equalsIgnoreCase(nome)){
+                    itensParaRemover.add(i);
+                }
+            }
+            carrinhoList.removeAll(itensParaRemover);
+        }
+        else {
+            System.out.println("A lista está vazia!");
+        }
+
+    }
+
+}
