aedea12973bc6e3b83a5fac1e956924e5887c73a
PC Thiago <thiago.hashimoto94@gmail.com>
2023-12-12 23:44:00 -0300

    List exercise from Collections classes by DIO

---
+++ src/PesquisaList/CatalogoLivros.java
@@ -0,0 +1,16 @@
+package PesquisaList;
+
+public class CatalogoLivros {
+    public void adicionarLivro(Livro livro){
+
+    }
+
+    public void pesquisarPorAutor(String autor){
+
+    }
+
+    public void pesquisarPorIntervaloAnos(int anoInicial, int anoFinal){
+
+    }
+
+}
